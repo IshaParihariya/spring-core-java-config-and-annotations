7b075ea56ccdfb76ffae9016e5ebef06ee4b70b6
IshaParihariya <ishh1036@gmail.com>
2026-04-14 23:42:17 +0530

    another object

--- src/main/java/main/LaunchApp.java
+++ src/main/java/main/LaunchApp.java
@@ -8,17 +8,37 @@ public class LaunchApp
 {
     public static void main(String[] args)
     {
+        // object 1
         // load spring container
         ApplicationContext context=new ClassPathXmlApplicationContext("applicationconfig.xml");
 
         // Get bean
-        Employee emp = (Employee) context.getBean("emp1");
+        Employee emp1 = (Employee) context.getBean("emp1");
 
         // printing toString method
-        System.out.println(emp.toString());
+        System.out.println(emp1.toString());
 
         // printing employeeTask method
-        System.out.println(emp.employeeTask());
+        System.out.println(emp1.employeeTask());
 
+
+        // object 2
+       Employee emp2=(Employee)context.getBean("emp2");
+
+        // printing toString method
+        System.out.println(emp2.toString());
+
+        // printing employeeTask method
+        System.out.println(emp2.employeeTask());
+
+
+        // object 3
+        Employee emp3=(Employee)context.getBean("emp3");
+
+        // printing toString method
+        System.out.println(emp3.toString());
+
+        // printing employeeTask method
+        System.out.println(emp3.employeeTask());
     }
 }
